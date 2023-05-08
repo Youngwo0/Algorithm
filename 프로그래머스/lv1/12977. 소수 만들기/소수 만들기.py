1e3551be080c05da2c8c50dda71918e741993cde
Youngwo0 <zerorain2@snu.ac.kr>
2023-05-08 11:34:25 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+
+def is_prime_number(n): #소수 판별 함수
+    if n == 0 or n == 1:
+        return False
+    else:
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+
+def solution(nums):
+    answer = 0
+
+    cmb = list(combinations(nums, 3))
+    sum_cmb = list(map(sum, cmb))
+
+    for a in sum_cmb:
+        if is_prime_number(a):
+            answer += 1
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution([1, 2, 7, 6, 4]))
